a70a072f7444e45c7c4d14ccdb17d74d13d18081
dip7501686040 <dip7501686040@gmail.com>
2024-12-27 10:02:19 +0530

    Added env files

--- src/apiHandlers/user.apiHandler.js
+++ src/apiHandlers/user.apiHandler.js
@@ -1,4 +1,4 @@
-const base_url = 'http://localhost:8001';
+const base_url = process.env.REACT_APP_GIMA_API_URL;
 export const getUserData = async (companycode = '', username = '') => {
   const response = await fetch(`${base_url}/api/v3/staff/bussinesscard/${username}?companycode=${companycode}`);
   const data = await response.json();
