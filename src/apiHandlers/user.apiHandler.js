938a30e91a05db7f8d5907fe945aaebed7521f27
Jigar Patel <patel.jigar8005@gmail.com>
2024-12-30 16:08:55 +0530

    base64 username

--- src/apiHandlers/user.apiHandler.js
+++ src/apiHandlers/user.apiHandler.js
@@ -1,6 +1,6 @@
 const base_url = process.env.REACT_APP_GIMA_API_URL;
 export const getUserData = async (companycode = '', username = '') => {
-  const response = await fetch(`${base_url}/api/v3/staff/bussinesscard/${username}?companycode=${companycode}`);
+  const response = await fetch(`${base_url}/api/v3/staff/bussinesscard/${atob(username)}?companycode=${companycode}`);
   const data = await response.json();
   return data;
 }
